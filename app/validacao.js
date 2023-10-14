9745a703c168e3959860ce0cdb696e77cecb4888
JPCadinelli <jpcadinelli@gmail.com>
2023-10-14 14:26:44 -0300

    curso finalizado

--- app/validacao.js
+++ app/validacao.js
@@ -1,10 +1,18 @@
 function verificaSeOChutePossiuUmValorValido(chute) {
-    const numero = parseInt(chute)
+    const numero = +chute
 
     if (Number.isNaN(numero)) {
-        elementoChute.innerHTML += `
-        <div>Valor inválido</div>
-        `
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
         return
     }
 
@@ -19,6 +27,7 @@ function verificaSeOChutePossiuUmValorValido(chute) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente!</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -30,3 +39,9 @@ function verificaSeOChutePossiuUmValorValido(chute) {
         `
     }
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
